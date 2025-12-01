d0304f8d3d3a037b572985213613a0fa53b244d7
Henrique Gatti <118054758+RickGatti@users.noreply.github.com>
2025-12-01 06:38:05 -0300

    Initial commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,9 @@
+@extends('partials.master')
+
+@section('title', 'Products List')
+
+@section('Products List')
+    <h1>Products List</h1>
+    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae quas quia aliquid harum porro? Mollitia deleniti vitae ducimus voluptates quam exercitationem dolore, omnis veniam, atque reiciendis tenetur! Itaque, est quasi.</h2>
+
+@endsection
